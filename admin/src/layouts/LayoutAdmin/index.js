507887b9e81ed70c9ec6588a04121e50496137b5
Đỗ Chí Thanh <thanhdohn2003@gmail.com>
2024-03-06 15:52:07 +0700

    Add directories C and D

---
+++ admin/src/layouts/LayoutAdmin/index.js
@@ -0,0 +1,22 @@
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+
+
+
+function LayoutAdmin() {
+    return ( 
+        <>
+        <Header/>
+        <Sidebar/>
+     
+
+        
+     
+  
+        </>
+     );
+}
+
+export default LayoutAdmin;
---
+++ smartele/src/layouts/LayoutSite/Login.js
@@ -0,0 +1,239 @@
+function Login() {
+    return ( 
+        <>
+  <div
+    className="modal fade"
+    id="signin-modal"
+    tabIndex={-1}
+    role="dialog"
+    aria-hidden="true"
+  >
+    <div className="modal-dialog modal-dialog-centered" role="document">
+      <div className="modal-content">
+        <div className="modal-body">
+          <button
+            type="button"
+            className="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">
+              <i className="icon-close" />
+            </span>
+          </button>
+          <div className="form-box">
+            <div className="form-tab">
+              <ul
+                className="nav nav-pills nav-fill nav-border-anim"
+                role="tablist"
+              >
+                <li className="nav-item">
+                  <a
+                    className="nav-link active"
+                    id="signin-tab"
+                    data-toggle="tab"
+                    href="#signin"
+                    role="tab"
+                    aria-controls="signin"
+                    aria-selected="true"
+                  >
+                    Đăng nhập
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    id="register-tab"
+                    data-toggle="tab"
+                    href="#register"
+                    role="tab"
+                    aria-controls="register"
+                    aria-selected="false"
+                  >
+                    Đăng ký
+                  </a>
+                </li>
+              </ul>
+              <div className="tab-content" id="tab-content-5">
+                <div
+                  className="tab-pane fade show active"
+                  id="signin"
+                  role="tabpanel"
+                  aria-labelledby="signin-tab"
+                >
+                  <form action="#">
+                    <div className="form-group">
+                      <label htmlFor="singin-email">
+                      Tên người dùng hoặc địa chỉ email *
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="singin-email"
+                        name="singin-email"
+                        required=""
+                      />
+                    </div>
+                    {/* End .form-group */}
+                    <div className="form-group">
+                      <label htmlFor="singin-password">Mật khẩu *</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="singin-password"
+                        name="singin-password"
+                        required=""
+                      />
+                    </div>
+                    {/* End .form-group */}
+                    <div className="form-footer">
+                      <button
+                        type="submit"
+                        className="btn btn-outline-primary-2"
+                      >
+                        <span>ĐĂNG NHẬP</span>
+                        <i className="icon-long-arrow-right" />
+                      </button>
+                      <div className="custom-control custom-checkbox">
+                        <input
+                          type="checkbox"
+                          className="custom-control-input"
+                          id="signin-remember"
+                        />
+                        <label
+                          className="custom-control-label"
+                          htmlFor="signin-remember"
+                        >
+                          Nhớ mật khẩu
+                        </label>
+                      </div>
+                      {/* End .custom-checkbox */}
+                      <a href="#" className="forgot-link">
+                        Quên mật khẩu?
+                      </a>
+                    </div>
+                    {/* End .form-footer */}
+                  </form>
+                  <div className="form-choice">
+                    <p className="text-center">hoặc đăng nhập</p>
+                    <div className="row">
+                      <div className="col-sm-6">
+                        <a href="#" className="btn btn-login btn-g">
+                          <i className="icon-google" />
+                          Đăng nhập với Google
+                        </a>
+                      </div>
+                      {/* End .col-6 */}
+                      <div className="col-sm-6">
+                        <a href="#" className="btn btn-login btn-f">
+                          <i className="icon-facebook-f" />
+                          Đăng nhập với Facebook
+                        </a>
+                      </div>
+                      {/* End .col-6 */}
+                    </div>
+                    {/* End .row */}
+                  </div>
+                  {/* End .form-choice */}
+                </div>
+                {/* .End .tab-pane */}
+                <div
+                  className="tab-pane fade"
+                  id="register"
+                  role="tabpanel"
+                  aria-labelledby="register-tab"
+                >
+                  <form action="#">
+                    <div className="form-group">
+                      <label htmlFor="register-email">
+                        Địa chỉ email của bạn *
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="register-email"
+                        name="register-email"
+                        required=""
+                      />
+                    </div>
+                    {/* End .form-group */}
+                    <div className="form-group">
+                      <label htmlFor="register-password">Mật khẩu *</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="register-password"
+                        name="register-password"
+                        required=""
+                      />
+                    </div>
+                    {/* End .form-group */}
+                    <div className="form-footer">
+                      <button
+                        type="submit"
+                        className="btn btn-outline-primary-2"
+                      >
+                        <span>ĐĂNG KÝ</span>
+                        <i className="icon-long-arrow-right" />
+                      </button>
+                      <div className="custom-control custom-checkbox">
+                        <input
+                          type="checkbox"
+                          className="custom-control-input"
+                          id="register-policy"
+                          required=""
+                        />
+                        <label
+                          className="custom-control-label"
+                          htmlFor="register-policy"
+                        >
+                          Tôi đồng ý với <a href="#">chính sách bảo mật</a> *
+                        </label>
+                      </div>
+                      {/* End .custom-checkbox */}
+                    </div>
+                    {/* End .form-footer */}
+                  </form>
+                  <div className="form-choice">
+                    <p className="text-center">hoặc đăng nhập</p>
+                    <div className="row">
+                      <div className="col-sm-6">
+                        <a href="#" className="btn btn-login btn-g">
+                          <i className="icon-google" />
+                          Đăng nhập với Google
+                        </a>
+                      </div>
+                      {/* End .col-6 */}
+                      <div className="col-sm-6">
+                        <a href="#" className="btn btn-login  btn-f">
+                          <i className="icon-facebook-f" />
+                          Đăng nhập với Facebook
+                        </a>
+                      </div>
+                      {/* End .col-6 */}
+                    </div>
+                    {/* End .row */}
+                  </div>
+                  {/* End .form-choice */}
+                </div>
+                {/* .End .tab-pane */}
+              </div>
+              {/* End .tab-content */}
+            </div>
+            {/* End .form-tab */}
+          </div>
+          {/* End .form-box */}
+        </div>
+        {/* End .modal-body */}
+      </div>
+      {/* End .modal-content */}
+    </div>
+    {/* End .modal-dialog */}
+  </div>
+  {/* End .modal */}
+</>
+
+     );
+}
+
+export default Login;
